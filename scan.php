3402da3a51d75f7849f16c3b397fb87a986b85e4
Benoit Duffez <benoit.duffez@upactivity.com>
2015-04-18 16:05:08 +0200

    prevent direct access to (wait-)scan.php, must be accessed through link on main page

--- scan.php
+++ scan.php
@@ -1,6 +1,10 @@
 <?php
+echo "Ref: '".print_r($_SERVER['HTTP_REFERER'],true)."'";
 
-$ret = exec('scan-one-image');
-print_r($ret);
+if (isset($_SERVER['HTTP_REFERER']) && preg_match('/wait-scan\.php$/', $_SERVER['HTTP_REFERER'])) {
+	$ret = exec('scan-one-image');
+	print_r($ret);
+}
+
 header('Location: /scan/');
 
--- wait-scan.php
+++ wait-scan.php
@@ -1,4 +1,12 @@
+<?php
+if (isset($_SERVER['HTTP_REFERER']) && preg_match('#/$#', $_SERVER['HTTP_REFERER'])) {
+	echo <<<HTML
 <script type="text/javascript">
 setTimeout("window.location = 'scan.php'", 100);
 </script>
 Scan en cours...
+HTML;
+} else {
+	header('Location: /scan/');
+}
+
